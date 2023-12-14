6839135064b82fb9e9dbb35dfe448e03cb4d5ee5
MYX <it.anton22@gmail.com>
2023-12-15 00:42:06 +0300

    finish task

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+import read from './reader';
+import json from './parser';
+
+export default class GameSavingLoader {
+  static load() {
+    return read()
+      .then((data) => json(data))
+      .then((jsonData) => JSON.parse(jsonData))
+      .then((gameSaving) => gameSaving);
+  }
+}
